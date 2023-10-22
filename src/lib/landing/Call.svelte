b48256f2af53c6a29629b2a4731fe1dd23abe358
a-zyk <austejazykute@gmail.com>
2023-10-22 15:10:56 +0300

    change tel number

--- src/lib/landing/Call.svelte
+++ src/lib/landing/Call.svelte
@@ -4,7 +4,7 @@
 
 <a
   on:click
-  href="tel:+370"
+  href="tel:+3703489593"
   class="bg-primary_blue cursor-pointer text-white z-50 p-5 mr-3 h-[63px] w-[63px] rounded-full fixed top-[300px] right-0 md:hidden"
   ><Phone /></a
 >
